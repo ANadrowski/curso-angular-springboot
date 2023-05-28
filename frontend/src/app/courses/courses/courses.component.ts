c5b57cb67d1a21528ecb66db72d21f853802ba60
Aislan Nadrowski <aislan.nadrowski@gmail.com>
2023-05-27 23:25:15 -0300

    Finalizada a criação do componente para exibição de erros

--- frontend/src/app/courses/courses/courses.component.ts
+++ frontend/src/app/courses/courses/courses.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { Course } from '../model/course';
 import { CoursesService } from '../services/courses.service';
 import { Observable, catchError, of } from 'rxjs';
+import { MatDialog } from '@angular/material/dialog';
+import { ErrorDialogComponent } from 'src/app/shared/components/error-dialog/error-dialog.component';
 
 
 @Component({
@@ -14,13 +16,22 @@ export class CoursesComponent {
   courses$: Observable<Course[]>;
   displayedColumns = ['name', 'category'];
 
-  constructor(private coursesService: CoursesService) {
+  constructor(
+    private coursesService: CoursesService,
+    public dialog: MatDialog
+  ) {
     this.courses$ = coursesService.list()
-    .pipe(
-      catchError(error => {
-        return of([]) 
-      })
-    );
+      .pipe(
+        catchError(error => {
+          this.onError('Erro ao carregar cursos.')
+          return of([])
+        })
+      );
   }
 
+  onError(erroMsg: string) {
+    this.dialog.open(ErrorDialogComponent, {
+      data: erroMsg,
+    });
+  }
 }
