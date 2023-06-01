975da3f285e041ca11290cbc75e0e6f5a492efba
Aislan Nadrowski <aislan.nadrowski@gmail.com>
2023-06-01 12:59:38 -0300

    Adicionada a anotação @PathVariable

--- backend/src/main/java/com/loiane/backend/controller/CourseController.java
+++ backend/src/main/java/com/loiane/backend/controller/CourseController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -27,10 +28,10 @@ public class CourseController {
         return courseRepository.findAll();
     }
 
-    /* 
+    /*  
     @GetMapping("/{id}")
-    public Course findById(Long id) {
-
+    public Course findById(@PathVariable Long id) {
+        return courseRepository.findById(id);
     }
     */
 
