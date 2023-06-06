f7e35d76a92fc0c04f731d2c148dbb56e9137a7a
Aislan Nadrowski <aislan.nadrowski@gmail.com>
2023-06-05 21:12:52 -0300

    Adicionando lógica para remoção logica - soft delete

--- backend/src/main/java/com/loiane/backend/model/Course.java
+++ backend/src/main/java/com/loiane/backend/model/Course.java
@@ -1,5 +1,7 @@
 package com.loiane.backend.model;
 
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
 import org.hibernate.validator.constraints.Length;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
@@ -16,6 +18,8 @@ import lombok.Data;
 
 @Data //Essa anotação gera os getters e setters (entre outras coisas)
 @Entity
+@SQLDelete(sql = "UPDATE Course SET status = 'Inativo' WHERE id = ?") //Essa anotação altera o comportamento do deleteById.
+@Where(clause = "status = 'Ativo'")
 public class Course {
     
     @Id
@@ -34,4 +38,10 @@ public class Course {
     @Pattern(regexp = "Back-end|Front-end")
     @Column(length = 10, nullable = false)
     private String category;
+
+    @NotNull 
+    @Length(max = 10)
+    @Pattern(regexp = "Ativo|Inativo")
+    @Column(length = 10, nullable = false)
+    private String status = "Ativo";
 }
--- frontend/src/app/shared/pipes/category.pipe.ts
+++ frontend/src/app/shared/pipes/category.pipe.ts
@@ -10,7 +10,7 @@ export class CategoryPipe implements PipeTransform {
       case 'Front-end' : return 'code'
       case 'Back-end' : return 'computer'
     }
-    
+
     return 'code';
   }
 
