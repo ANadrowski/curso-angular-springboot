a4d0e45da122d129552091aa0b716946f23cbd4f
Aislan Nadrowski <aislan.nadrowski@gmail.com>
2023-06-08 11:20:12 -0300

    Começando a adicionar um DTO usando Java Record

---
+++ backend/src/main/java/com/loiane/backend/dto/CourseDTO.java
@@ -0,0 +1,26 @@
+package com.loiane.backend.dto;
+
+import org.hibernate.validator.constraints.Length;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+
+/*
+ * Este é um record. Está disponível a partir do Java 14,
+ * No record, todos os dados devem ser passados dento do construtor,
+ * e não existe setters. Já a recuperação dos dados podem ser feitas
+ * através do getters.
+ * Portanto, trata-se de uma classe imutável do Java.
+ * Outra diferença para uma classe normal é que os métodos 
+ * getters não usam o prefixo get, devemos acessar diretamente
+ * pelo nome da propriedade.
+ */
+
+public record CourseDTO(
+    @JsonProperty("_id") Long id, 
+    @NotBlank @NotNull @Length(min = 5, max = 100) String name, 
+    @NotBlank @NotNull @Length(max = 10) @Pattern(regexp = "Back-end|Front-end") String category) {
+}
